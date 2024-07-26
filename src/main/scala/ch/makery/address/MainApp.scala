024b6a9bb375d960afe40af4b03c7e0510ff566e
Yap Jay Ann <yapjayann@gmail.com>
2024-07-26 16:49:47 +0800

    changed MainApp

--- src/main/scala/ch/makery/address/MainApp.scala
+++ src/main/scala/ch/makery/address/MainApp.scala
@@ -1,5 +1,22 @@
 package ch.makery.address
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.Includes._
+import scalafxml.core.{NoDependencyResolver, FXMLView, FXMLLoader}
+import javafx.{scene => jfxs}
 
-object MainApp {
+object MainApp extends JFXApp {
+  // transform path of RootLayout.fxml to URI for resource location.
 
+  val rootResource = getClass.getResource("view/HomePage.fxml")
+  val loader = new FXMLLoader(rootResource, NoDependencyResolver)
+  loader.load();
+  val roots = loader.getRoot[jfxs.layout.AnchorPane]
+  stage = new PrimaryStage {
+    title = "Dinosaur game"
+    scene = new Scene {
+      root = roots
+    }
+  }
 }
