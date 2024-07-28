efec9d655fd08330a8846f6c9507fc57a0976c96
Yap Jay Ann <yapjayann@gmail.com>
2024-07-29 04:03:36 +0800

    override methods in ClothingItem

--- src/main/scala/ch/makery/address/model/ClothingItem.scala
+++ src/main/scala/ch/makery/address/model/ClothingItem.scala
@@ -4,14 +4,24 @@ abstract class ClothingItem(val id: String) extends Sellable{
   var name: String = _
   var quantity: Int = _
   var price: Double = _
+
+  def save(): Unit ={
+
+  }
 }
 
 class Dress (_id: String)extends ClothingItem(_id){
   var size: Char = _
-  //override def
+  //override def save
+  override def save(): Unit = {
+
+  }
 }
 
 class Accessory(_id: String) extends ClothingItem(_id){
   var material: String = _
   //override def
+  override def save(): Unit = {
+
+  }
 }
