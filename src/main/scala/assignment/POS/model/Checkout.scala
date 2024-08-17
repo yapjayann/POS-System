acbf806651246ffb37d0b21a367bd4eaa7e6984b
Yap Jay Ann <yapjayann@gmail.com>
2024-08-18 05:18:48 +0800

    added comments

--- src/main/scala/assignment/POS/model/Checkout.scala
+++ src/main/scala/assignment/POS/model/Checkout.scala
@@ -1,10 +1,13 @@
 package assignment.POS.model
+import scala.collection.immutable.List
 
-// Case class to represent Voucher
+// Case class to represent a Voucher
+// 'apply' take total amount as input and returns the discounted amount
 case class Voucher(name: String, apply: Double => Double)
 
 // For Checkout logic
 class Checkout {
+  // A list of predefined vouchers available
   val vouchers: List[Voucher] = List(
     Voucher("No Discount", amount => amount),
     Voucher("$20 Off", amount => Math.max(amount - 20, 0)),
@@ -12,10 +15,12 @@ class Checkout {
     Voucher("15% Discount", amount => amount * 0.85)
   )
 
+  // Calculate total after applying the voucher
   def calculateTotal(amount: Double, voucher: Voucher): Double = {
     Math.max(voucher.apply(amount), 0)
   }
 
+  // Calculate any change (paid-total)
   def calculateChange(total: Double, paid: Double): Double = {
     Math.max(paid - total, 0)
   }
